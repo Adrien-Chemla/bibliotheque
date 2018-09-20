755154df95b8aab5ee4d837f1250e8d43fb9c968
Adrien Chemla <chemladrien@gmail.com>
2018-09-20 18:55:58 +0200

    prepare add DetailFilm

--- components/FilmDetail.js
+++ components/FilmDetail.js
@@ -2,10 +2,29 @@ import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
 
 class FilmDetail extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      film: undefined, 
+      isLoading: true 
+    }
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      // Si isLoading vaut true, on affiche le chargement à l'écran
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+        </View>
+      )
+    }
+  }
+
   render() {
     return (
       <View style={styles.main_container}>
-        <Text>Détail du film</Text>
+        {this._displayLoading()}
       </View>
     )
   }
--- components/Search.js
+++ components/Search.js
@@ -18,7 +18,7 @@ class Search extends React.Component {
 
   _displayDetailForFilm = (idFilm) => {
     console.log("Display film with id " + idFilm)
-    this.props.navigation.navigate("FilmDetail")
+    this.props.navigation.navigate("FilmDetail", { idFilm: idFilm })
   }
 
   _loadFilms() {
